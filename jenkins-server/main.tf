5c8d8500d3cd92741261def7ec9756336fcd082c
Aman Duggal <amanduggal103@gmail.com>
2024-05-29 00:54:04 +0530

    Update main.tf

--- jenkins-server/main.tf
+++ jenkins-server/main.tf
@@ -4,57 +4,88 @@ resource "random_id" "bucket_suffix" {
 }
 
 resource "aws_s3_bucket" "s3bucket" {
-  bucket = "eks-cluster-bucket-${random_id.bucket_suffix.hex}"
+  bucket = "jenkins-server-bucket-${random_id.bucket_suffix.hex}"
 }
 
 #VPC
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "eks-cluster-vpc"
+  name = "jenkins-vpc"
   cidr = var.vpc_cidr
 
   azs             = data.aws_availability_zones.azs.names
   public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
   enable_dns_hostnames = true
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  map_public_ip_on_launch = true
 
   tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    Name = "Jenkins VPC"
+    Terraform = "true"
+    Environment = "dev"
   }
+
   public_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"               = 1
-
-  }
-  private_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/private_elb"       = 1
-
+    Name = "Jenkins Subnet"
   }
 }
 
-#EKS
-module "eks" {
-  source                         = "terraform-aws-modules/eks/aws"
-  cluster_name                   = "my-eks-cluster"
-  cluster_version                = "1.29"
-  cluster_endpoint_public_access = true
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+# Security Group
+resource "aws_security_group" "jenkins_sg" {
+  name        = "Jenkins-SecurityGroup"
+  description = "Security group for Jenkins"
+  vpc_id      = module.vpc.vpc_id
 
-  eks_managed_node_groups = {
-    nodes = {
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
-      instance_types = var.instance_types
-    }
-  }
   tags = {
-    Environment = "dev"
+    Name = "Jenkins-SecurityGroup"
+  }
+}
+
+resource "aws_security_group_rule" "ingress_http" {
+  type            = "ingress"
+  from_port       = 8080
+  to_port         = 8080
+  protocol        = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.jenkins_sg.id
+  description     = "HTTP"
+}
+
+resource "aws_security_group_rule" "ingress_ssh" {
+  type            = "ingress"
+  from_port       = 22
+  to_port         = 22
+  protocol        = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.jenkins_sg.id
+  description     = "SSH"
+}
+
+resource "aws_security_group_rule" "egress" {
+  type            = "egress"
+  from_port       = 0
+  to_port         = 0
+  protocol        = "-1"
+  cidr_blocks     = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.jenkins_sg.id
+}
+
+
+# EC2
+
+resource "aws_instance" "jenkins-server" {
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  availability_zone           = data.aws_availability_zones.azs.names[0]
+  user_data                   = file("jenkins-install.sh")
+
+  tags = {
+    Name        = "Jenkins-Server-New"
     Terraform   = "true"
+    Environment = "dev"
   }
 }
