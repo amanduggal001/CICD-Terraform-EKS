df3866ddaca64dd04b80eaec605e14561f1dc03c
Aman Duggal <amanduggal103@gmail.com>
2024-05-29 00:49:54 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
-    key    = "eks/terraform.tfstate"
-    region = "us-east-2"
+    bucket = "jenkins-server-bucket-4e6aeaee"
+    key = "eks/terraform.tfstate"
+    region = "us-east-1"
   }
+
 }
